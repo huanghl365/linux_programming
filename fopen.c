d44dd19db7c8ee2d6af6bf90ce3cf78706ea9d65
flyingwords <maxucan163@163.com>
2020-02-22 08:08:39 +0800

    commit

--- fopen.c
+++ fopen.c
@@ -8,9 +8,29 @@
 #include <string.h>
 #define READ_NUM  1024
 
+
+/*
+函数原型：FILE *fopen(const char *path, const char *mode);
+描述：打开名称为path指向的字符串的文件，并将流与其关联。
+参数：
+path：指向文件的路径
+mode：
+r rb 以只读方式打开文件，文件必须存在
+w wb 只写方式打开文件，并把文件截短为零，文件不存在则创建
+a ab 以写方式打开文件，新内容追加文件尾，文件不存在则创建
+
+r+ rb+ 以更新方式打开（读和写），文件必须存在
+w+ wb+ 以更新方式打开文件，并把文件长度截短为零，文件不存在则创建
+a+ ab+ 以更新方式打开，新内容追加文件尾，文件不存在则创建
+返回值：
+调用成功则返回文件指针，失败则返回NULL，并设置errno
+*/
+
+
 /*
 程序功能说明：简单测试fopen函数
 */
+
 int main(void)
 {
 	FILE *fp1, *fp2;  //指针前面一定加上*
@@ -18,7 +38,7 @@ int main(void)
 	int read_cnt , write_cnt;
 	fp1 = fopen("test1.txt", "a+");   			 //更新方式打开，接续写
 	//fp1 = fopen("./time_operation.c", "w+");  //更新方式打开，长度截短为0
-	fp2 = fopen("test2.txt", "r");   			//读方式打开
+	fp2 = fopen("test2.txt", "r+");   			//读方式打开
 	
 	
 	if (fp1 != NULL && fp2 != NULL)
@@ -33,6 +53,7 @@ int main(void)
 	else
 	{
 		printf("fopen file failed\n");
+		return 1;
 	}
 	fclose(fp1);//关闭文件，隐含一次fflush
 	fclose(fp2);
--- write_read.c
+++ write_read.c
@@ -7,6 +7,34 @@
 #include <stdio.h>
 #include <string.h>
 
+/*
+函数原型：ssize_t read(int fd, void *buf, size_t count);
+描述：read用来将文件描述符fd中读取count个字符取到缓冲区buf中
+参数：
+fd：文件描述符
+buf：用来存储读取字节的缓冲区
+count：从文件中读取的字节总数
+返回值：
+返回读取的字节数，并且文件当前的位置会偏移返回的字节数大小。
+如果读取的字节数小于指定的字节总数count，并不一定代表错误（可能到达文件尾等原因造成）。
+如果读取错误，则返回-1，并设置对应的errno.
+
+
+函数原型：ssize_t write(int fd, const void *buf, size_t count);
+描述：write用来将缓冲区buf的count个字节写入到文件描述符fd指定的文件中。
+如果使用O_APPEND打开文件，则在写入之前会先将文件偏移到文件的末尾。
+参数：
+fd：文件描述符
+buf：用来写入文件的字符缓冲区
+count：指定要写入到文件的字节总数
+返回值：
+返回写入的字节数，并且文件当前的位置会偏移实际写入的字节数目的大小。
+如果写入的字节数小于指定的字节总数count，并不一定代表错误（可能是因为磁盘空间不够造成）。
+如果写入错误，则返回-1，并设置对应的errno。
+
+注意：写入数据的时候，需要写入多少数据就指定多大的数目，如果指定的count大小超过缓冲区的大小，
+还是按照指定的大小输入数据，这样数组就会越界，写入的数据是不可知的。
+*/
 
 /*
 程序功能描述：使用read write操作 标准输入输出的文件描述符
@@ -31,9 +59,9 @@ int main(void)
 
 
 /*
-程序功能描述：使用write写数据到空文件，并使用lseek定位读取出来
+程序功能描述：使用write写数据到空文件，并使用lseek定位后使用read读取出来
 */
-#if 0
+#if 1
 int main(void)
 {
 	int fd = -1;
@@ -49,13 +77,11 @@ int main(void)
 
 	lseek(fd, 0, SEEK_END);
 
-	//write是指定写入多大就写入多大，如果指定的范围超过数组的大小，还是按照指定的大小输入数据，这样数组就会越界，写入的数据是不可知的
-	//read并不是指定读取多少就一定能读取多少，在不超过指定的大小的情况下，能读取多少就读取多少，跟文件的数据量大小有关
-	
 	//使用wirte写数据时，应该使用strlen指定数据大小，不要使用sizeof，因为这样有可能会写入很多空字符
-	//ret = write(fd, write_buf, sizeof(write_buf));
+	//如果指定的范围超过数组的大小，还是按照指定的大小输入数据，这样数组就会越界，写入的数据是不可知的
+	ret = write(fd, write_buf, sizeof(write_buf));
 	//ret = write(fd, write_buf, strlen(write_buf));
-	ret = write(fd, write_buf, 100);
+	//ret = write(fd, write_buf, 100);
 
 	if (-1 != ret)
 	{
@@ -70,8 +96,8 @@ int main(void)
 	
 
 	lseek(fd, 0, SEEK_SET);
-	//ret = read(fd, read_buf, ret);
-	ret = read(fd, read_buf, 100);
+	ret = read(fd, read_buf, ret);
+	//ret = read(fd, read_buf, 100);
 
 	if (-1 != ret)
 	{
