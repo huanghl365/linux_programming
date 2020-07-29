2d54165541b79899a8032c8fd27d18e92f532987
flyingwords <maxucan163@163.com>
2020-07-29 22:32:54 +0800

    cm

--- stray_knowledge/atexit.c
+++ stray_knowledge/atexit.c
@@ -20,12 +20,11 @@ void fun2(void)
 
 int main(void)
 {
-	//atexit注册进程终止处理函数,先注册的后执行,当
-	//在main中执行return或者程序使用了exit调用时，会执行回调函数
+	/*测试：atexit注册进程终止处理函数,先注册的后执行,当在main中执行return或者程序使用了exit调用时，会执行回调函数
+	测试结果：先打印hello world再执行进程终止函数*/
 	atexit(fun1);
 	atexit(fun2);
 	printf("hello world\n");
 	return 0;
-	//测试结果：先打印hello world再执行进程终止函数
 }
 
