6d452801dafac2d22fdb0dc6e319668cdb9b4649
flyingwords <maxucan163@163.com>
2020-08-13 09:16:59 +0800

    cm

--- process/pipe/pipe.c
+++ process/pipe/pipe.c
@@ -52,7 +52,53 @@ int main(void)
 		printf("进程复制失败\n");
 		exit(1);
 	}
+
+/*
+测试：测试通过一个管道进行父子进程单向数据通信
+*/
+
+#if 1
+		if (0 == pid) 
+		{
+			close(pipe_fd[0]); //关闭读端
+			
+			sprintf(write_buf, "I am child process, my pid is %d", getpid());
+			write(pipe_fd[1], write_buf, strlen(write_buf));		
+			exit(0);
+		}
 	
+		if (pid > 0)
+		{
+			close(pipe_fd[1]); //关闭写端
+			ret = read(pipe_fd[0], read_buf, sizeof(read_buf)-1);			  
+			if (ret != -1)
+			{
+				printf("ret = %d\n",ret);
+				printf("read from pipe: %s\n", read_buf);
+			}
+			else
+			{
+				perror("read");
+				exit(1);
+			}
+			
+			pid = waitpid(pid, &status, 0); 
+			if (-1 == pid)
+			{
+				perror("waitpid");
+				exit(1);
+			}
+			
+		}
+#endif
+
+
+/*
+测试通过一个管道进行父子进程双向通信
+测试结果：管道作为单向数据通道，不应该用一个管道进行父子进程双向通信，
+容易造成通信数据错乱，正确的做法是使用两个管道做双向通信。
+*/
+#if 0
 	if (0 == pid) 
 	{
 		//子进程向管道写入数据，父进程从管道读取数据
@@ -61,7 +107,7 @@ int main(void)
 
 
 		//延时1s再读，避免子进程读取了子进程写入的数据，导致父进程读取不到卡死
-		sleep(1);	
+		//sleep(1);	
 
 
 		//父进程向管道写入数据，子进程从管道读取数据
@@ -81,7 +127,6 @@ int main(void)
 
 	if (pid > 0)
 	{
-		printf("child pid: %d\n", pid);
 		ret = read(pipe_fd[0], read_buf, sizeof(read_buf)-1); 			  
 		if (ret != -1)
 		{
@@ -97,7 +142,7 @@ int main(void)
 		sprintf(write_buf, "I am parent process, my pid is %d", getpid()); 
 		write(pipe_fd[1], write_buf, strlen(write_buf)); 
 		
-		pid = waitpid(pid, &status, 0);  //阻塞回收子进程
+		pid = waitpid(pid, &status, 0);  
 		if (-1 == pid)
 		{
 			perror("waitpid");
@@ -105,7 +150,9 @@ int main(void)
 		}
 		
 	}
-	
+#endif
+
+
 	return 0;
 }
 
